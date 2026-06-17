a93e96ff7337add6634f4076df2b0e0a5e350fc5
leontievasophia <leontievasophia@gmail.com>
2026-06-18 00:18:57 +0300

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,43 @@
-const forM=document.querySelector('.feedback-form');
+const forM = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-let formData={
-    email: "",
-    message: "" ,
 
+let formData = {
+  email: '',
+  message: '',
 };
 
-forM.addEventListener('input', (el)=>{
+forM.addEventListener('input', event => {
+  const fieldName = event.target.name;
+  const fieldValue = event.target.value.trim();
 
-localStorage.setItem('formData', JSON.stringify(formData));
+  formData[fieldName] = fieldValue;
 
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+const data=localStorage.getItem(STORAGE_KEY);
+
+    if(data){
+        const parseData = JSON.parse(data);
+        formData.email=parseData.email;
+        formData.message=parseData.message;
+
+        forM.elements.email.value=formData.email;
+        forM.elements.message.value=formData.message;
+    };
+   
+
+forM.addEventListener('submit', event => {
+    event.preventDefault();
+    if(formData.email === '' || formData.message === ''){
+        alert('Fill please all fields');
+    }
+    else{
+        console.log(formData);
+        localStorage.removeItem(STORAGE_KEY);
+        forM.reset();
+        formData.email = '';
+        formData.message = ''; 
+    }
 });
